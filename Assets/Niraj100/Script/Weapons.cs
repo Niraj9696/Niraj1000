84c87a35a11c50c51e4b6466ba0e76a6919135e3
Niraj9696 <pklniraj10@gmail.com>
2024-12-07 20:35:55 +0200

    gun with flash

--- Assets/Niraj100/Script/Weapons.cs
+++ Assets/Niraj100/Script/Weapons.cs
@@ -1,16 +1,30 @@
-using System.Collections;
+ 
+using StarterAssets;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
+    StarterAssetsInputs starterAssetsInputs;
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
     
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (starterAssetsInputs.shoot)
         {
+        
+            RaycastHit hit;
+        
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+       
+            {
             Debug.Log(hit.collider.name);
+        
+            }
+
         }
     }
 }
--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool shoot;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,6 +44,10 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+		public void OnShoot(InputValue value)
+		{
+		    ShootInput(value.isPressed);
+		}
 #endif
 
 
@@ -65,7 +70,10 @@ namespace StarterAssets
 		{
 			sprint = newSprintState;
 		}
-		
+		 public void ShootInput(bool newShootState)
+		{
+			shoot = newShootState;
+		}
 		private void OnApplicationFocus(bool hasFocus)
 		{
 			SetCursorState(cursorLocked);
